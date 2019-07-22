305a7e64e0d949953717eaac90111e3db6c4d132
LiangYT <381518188@qq.com>
2019-07-22 15:57:24 +0800

    优化会议详情！

--- app/src/main/java/com/zkhy/webrtc/android/HomeDemoActivity.java
+++ app/src/main/java/com/zkhy/webrtc/android/HomeDemoActivity.java
@@ -3,7 +3,6 @@ package com.zkhy.webrtc.android;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
